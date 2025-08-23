96d0a84d795b533b4eb0fb0d1e5d8871d92afbbe
Edward <eric199703@hotmail.com>
2025-08-23 09:35:17 -0300

    estilizando a sidebar

--- src/containers/Sidebar/index.tsx
+++ src/containers/Sidebar/index.tsx
@@ -1,9 +1,25 @@
+import Avatar from '../../components/Avatar'
+import Paragrafo from '../../components/Paragrafo'
 import Titulo from '../../components/Titulo'
+
+import { Descricao, BotaoTema, SidebarContainer } from './styles'
 
 const Sidebar = () => (
   <aside>
-    <img src="https://github.com/EdwardEricV.png" />
-    <Titulo fontSize={20}>Edward Eric</Titulo>
+    <SidebarContainer>
+      <Avatar />
+      <Titulo fontSize={20}>Edward Eric</Titulo>
+      <Paragrafo tipo="secundario" fontSize={16}>
+        EdwardEricV
+      </Paragrafo>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro de Computção
+      </Descricao>
+      <Descricao tipo="principal" fontSize={12}>
+        Engenheiro front-end
+      </Descricao>
+      <BotaoTema>Trocar tema</BotaoTema>
+    </SidebarContainer>
   </aside>
 )
 
