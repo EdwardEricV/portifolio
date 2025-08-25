abccc1c8be743358788e28d60b7e7e8d8e8b47ff
Edward <eric199703@hotmail.com>
2025-08-25 15:05:49 -0300

    FInalizando o projeto com os temas

--- src/containers/Sobre/index.tsx
+++ src/containers/Sobre/index.tsx
@@ -12,7 +12,7 @@ const Sobre = () => (
       aliquam vitae.
     </Paragrafo>
     <GithubSecao>
-      <img src="https://github-readme-stats.vercel.app/api?username=cristianotobias&show_icons=true&theme=dracula&include_all_commits=true&count_private=true" />
+      <img src="https://github-readme-stats.vercel.app/api?username=EdwardEricV&show_icons=true&theme=dracula&count_private=true" />
       <img src="https://github-readme-stats.vercel.app/api/top-langs/?username=EdwardEricV&layout=compact&langs_count=7&theme=dracula" />
     </GithubSecao>
   </section>
